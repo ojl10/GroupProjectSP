f0cf6f7728c745dd79b7acc0ad38102be0df2b0e
Owen <owen_lea@hotmail.co.uk>
2019-03-16 23:41:52 +0000

    Added new puzzle script
    push an object into a position to activate another object, disable it or create it.

---
+++ 3rdPerson/Assets/Owens Scripts/PuzzleInteractObj.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleInteractObj : MonoBehaviour, Iinteract
+{
+    MeshRenderer myMesh;
+    Collider mCollider;
+
+    //Need to add ability to swap if object disapears or appears
+    //add a timer potentially
+
+    void Start()
+    {
+        myMesh = GetComponent<MeshRenderer>();
+        mCollider = GetComponent<Collider>();
+        myMesh.enabled = false;
+        mCollider.enabled = false;
+    }
+
+    public void Interacted()
+    {
+        myMesh.enabled = true;
+        mCollider.enabled = true;
+    }
+
+
+}
---
+++ 3rdPerson/Assets/Owens Scripts/PuzzleObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleObject : MonoBehaviour
+{
+    public GameObject ActivatorObj;//object that will activate an event
+    public GameObject ActivatedObj;//object that will be triggered
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PuzzlePiece")
+        {
+            ActivatorObj = other.gameObject;
+            Activated();
+        }
+        else
+        {
+
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        ActivatorObj = null;
+    }
+
+    void Activated()
+    {
+        Iinteract interacting = ActivatedObj.gameObject.GetComponent<Iinteract>(); //calls a interaction on the activatable object.
+        interacting.Interacted();
+    }
+}
