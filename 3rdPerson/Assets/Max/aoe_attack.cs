8dce41ef201bdc47e678a9002452b268b12c34c1
qulatro <48255836+qulatro@users.noreply.github.com>
2019-03-20 20:00:50 +0000

    Max's Level & AOE script

---
+++ 3rdPerson/Assets/Max/aoe_attack.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aoe_attack : MonoBehaviour
+{
+    public GameObject aoeObject;
+
+    public float delay = 5f;
+
+    public float sec = 1f;
+
+    float uptime;
+
+    float cooldown;
+
+    bool canhit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cooldown = delay;
+
+        aoeObject.SetActive(false);
+
+        uptime = sec;
+
+        canhit = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((canhit == true) && (Input.GetKeyDown(KeyCode.Q)))
+        {
+            aoeObject.SetActive(true);
+            Debug.Log("has hit");
+        }
+
+        if (aoeObject.activeInHierarchy == true)
+        {
+            uptime -= Time.deltaTime;
+        }
+
+        if (uptime <= 0f)
+        {
+            aoeObject.SetActive(false);
+            uptime = sec;
+            canhit = false;
+        }
+
+        if ((uptime == 1f) && (canhit ==false))
+        {
+            cooldown -= Time.deltaTime;
+        }
+
+        if (cooldown <= 0f)
+        {
+            canhit = true;
+            cooldown = delay;
+        }
+    }
+}
