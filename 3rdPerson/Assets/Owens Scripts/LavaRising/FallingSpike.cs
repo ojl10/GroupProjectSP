1e7474a947c14a25423d50e5b976b3ac2133642b
Owen <owen_lea@hotmail.co.uk>
2019-03-28 01:12:10 +0000

    Updated Lava area and falling spike

---
+++ 3rdPerson/Assets/Owens Scripts/LavaRising/FallingSpike.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingSpike : MonoBehaviour
+{
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            animator.SetTrigger("Fall");
+        }
+        
+    }
+}
